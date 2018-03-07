25707eefba889bdbd5b6674e6fedd78aa9bd921b
fangwei <531614842@qq.com>
2018-03-07 14:26:45 +0800

    添加热加载

--- .webpackrc.js
+++ .webpackrc.js
@@ -2,5 +2,12 @@ module.exports = {
   entry: {
     main: './src/index.js',
   },
-  html: { template: './src/index.ejs' }
+  html: { template: './src/index.ejs' },
+
+  // 热加载
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+    },
+  },
 }
